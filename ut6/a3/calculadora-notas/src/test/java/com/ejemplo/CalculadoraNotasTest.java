e81df866a5f9b146d0280a89c9fa7f140998bc08
IvnMD <ivan.mesa.dominguez@proton.me>
2026-04-15 19:16:31 +0100

    UT6/A3 casi terminado

---
+++ ut6/a3/calculadora-notas/src/test/java/com/ejemplo/CalculadoraNotasTest.java
@@ -0,0 +1,39 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class CalculadoraNotasTest {
+
+    @Test
+    void testMediaSimple() {
+        assertEquals(7, CalculadoraNotas.calcularMedia(new int[] { 6, 7, 8 }));
+    }
+
+    @Test
+    void testMediaDecimal() {
+        assertEquals(8.5, CalculadoraNotas.calcularMedia(new int[] { 10, 9, 8, 7 }));
+    }
+
+    @Test
+    void testListaVacia() {
+        IllegalArgumentException excepcion = assertThrows(IllegalArgumentException.class, () -> {
+            CalculadoraNotas.calcularMedia(new int[] {});
+        });
+        assertEquals("Lista vacía", excepcion.getMessage());
+    }
+
+    @Test
+    void testNotaFueraRango() {
+        IllegalArgumentException excepcion = assertThrows(IllegalArgumentException.class, () -> {
+            CalculadoraNotas.calcularMedia(new int[] {11});
+        });
+        assertEquals("Nota fuera de rango", excepcion.getMessage());
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            CalculadoraNotas.calcularMedia(new int[] {-2});
+        });
+        assertEquals("Nota fuera de rango", excepcion.getMessage());
+    }
+
+    
+
+}
